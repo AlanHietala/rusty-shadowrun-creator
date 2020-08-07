a17df0291ec92ddbc7916a5be9b766e982df6355
Alan Hietala <alan.hietala@gmail.com>
2020-08-07 13:02:07 -0400

    updating tests and formatting

--- src/character.rs
+++ src/character.rs
@@ -1,11 +1,11 @@
-use enum_map::{enum_map, Enum, EnumMap};
 use enum_iterator::IntoEnumIterator;
+use enum_map::{enum_map, Enum, EnumMap};
 
 pub fn init_metatypes() -> EnumMap<MetaTypes, EnumMap<Attributes, AttributeRanges>> {
   let metatype_data = enum_map! {
     MetaTypes::Human => enum_map! {
       Attributes::Body => AttributeRanges {
-        min: 1, 
+        min: 1,
         max: 6,
       }
     }
@@ -17,36 +17,33 @@ pub fn init_metatypes() -> EnumMap<MetaTypes, EnumMap<Attributes, AttributeRange
 struct Character {
   attribute_points_to_add: u32,
   attribute_points_added: EnumMap<Attributes, u32>,
-  attribute_ranges: EnumMap<Attributes, AttributeRanges>
+  attribute_ranges: EnumMap<Attributes, AttributeRanges>,
 }
 
 impl Character {
-  fn can_increase_attribute(&mut self, attribute: Attributes) {
-  }
+  fn can_increase_attribute(&mut self, attribute: Attributes) {}
 
   fn is_attribute_at_max(&mut self) -> bool {
-
     let attributes = Attributes::into_enum_iter();
     let mut is_attribute_at_max = false;
     for attribute in attributes {
-      let AttributeRanges {max, min} = self.attribute_ranges[attribute];
+      let AttributeRanges { max, min } = self.attribute_ranges[attribute];
       let added = self.attribute_points_added[attribute];
 
       if min + added == max {
         is_attribute_at_max = true;
         break;
       }
-
     }
 
     return is_attribute_at_max;
   }
 
   fn increase_attribute(&mut self, attribute: Attributes) {
-      if !self.is_attribute_at_max() {
-        self.attribute_points_added[attribute] += 1;
-      }
-  }  
+    if !self.is_attribute_at_max() {
+      self.attribute_points_added[attribute] += 1;
+    }
+  }
 }
 
 #[derive(Enum, IntoEnumIterator, Copy, Clone, Debug)]
--- tests/integration_character.rs
+++ tests/integration_character.rs
@@ -1,11 +1,17 @@
-
-use rustysrlib::character::{init_metatypes, MetaTypes, Attributes};
-
+use rustysrlib::character::{init_metatypes, Attributes, MetaTypes};
 
 #[test]
 fn it_inits_humans_correctly() {
     let expected_min = 1;
     let expected_max = 6;
 
-    assert_eq!(expected_min, rustysrlib::character::init_metatypes()[MetaTypes::Human][Attributes::Body].min);
+    assert_eq!(
+        expected_min,
+        init_metatypes()[MetaTypes::Human][Attributes::Body].min
+    );
+
+    assert_eq!(
+        expected_max,
+        init_metatypes()[MetaTypes::Human][Attributes::Body].max
+    );
 }
